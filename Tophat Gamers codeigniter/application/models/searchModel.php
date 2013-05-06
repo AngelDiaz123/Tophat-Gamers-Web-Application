3f382884cd131521a45610973d1f40ff9cd310b6
Angel Diaz <surferdie141@fullsail.edu>
2013-05-06 17:59:03 -0400

    multiple areas of data
    most of the data pulled from the search field and returned

--- Tophat Gamers codeigniter/application/models/searchModel.php
+++ Tophat Gamers codeigniter/application/models/searchModel.php
@@ -4,6 +4,8 @@
 
       $this -> db -> select('username');
       $this -> db -> from('users');
+      $this -> db -> join('profile','users.user_id = profile.user_id');
+      $this -> db -> select('bio,user_img');
 
       $query = $this -> db -> get();
       return $query->result_array();
