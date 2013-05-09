1578ba16c80e53b9b51574f4fdfb3d4122089091
Angel Diaz <surferdie141@fullsail.edu>
2013-05-08 20:45:49 -0400

    updated home page css

--- Tophat Gamers codeigniter/application/views/welcome_view.php
+++ Tophat Gamers codeigniter/application/views/welcome_view.php
@@ -42,24 +42,24 @@
 		</header>
 		<img src="<?php echo base_url('images/controllers.jpg'); ?>" id="background_img_home">
 	</div>
-	<div class="home_banner"></div>
 
 	<section id="home_content">
-		<div class="group_img circles">
-			<img src="<?php echo base_url('images/people.png') ?>" class="circle_people">
-			<p class="people_text">Come and interact with people that have the same love for games that you do.</p>
-		</div>
-		<div class="group_file circles">
-			<img src="<?php echo base_url('images/file.png') ?>" class="circle_file">
-			<p class="file_text">Let others read your ideas by creating blogs for others on the site.</p>
-		</div>
-		<div class="group_play circles">
-			<img src="<?php echo base_url('images/play_img.png') ?>" class="circle_play">
-			<p class="play_text">Watch videos from other users to catch up on their interest and gaming styles.</p>
+		<div class="holder">
+			<div class="group_img circles">
+				<img src="<?php echo base_url('images/people.png') ?>" class="circle_people">
+				<p class="people_text">Come and interact with people that have the same love for games that you do.</p>
+			</div>
+			<div class="group_file circles">
+				<img src="<?php echo base_url('images/file.png') ?>" class="circle_file">
+				<p class="file_text">Let others read your ideas by creating blogs for others on the site.</p>
+			</div>
+			<div class="group_play circles">
+				<img src="<?php echo base_url('images/play_img.png') ?>" class="circle_play">
+				<p class="play_text">Watch videos from other users to catch up on their interest and gaming styles.</p>
+			</div>
 		</div>
 	</section>
 
-	<div class="footer_banner"></div>
 	<div class="footer_background">
 		<footer>
 			<a href="#" class="about">About Us</a>
