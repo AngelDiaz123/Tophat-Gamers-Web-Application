4813c6ae08090e35d247cf2fef289f23623a8cd1
Angel Diaz <surferdie141@fullsail.edu>
2013-05-31 13:30:36 -0400

    logged in about updated

--- Tophat Gamers codeigniter/application/views/aboutloggedin_view.php
+++ Tophat Gamers codeigniter/application/views/aboutloggedin_view.php
@@ -5,6 +5,9 @@
 	<link href="<?php echo base_url('images/favicon.ico'); ?>" rel="icon" type="image/x-icon" />
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700|Didact+Gothic' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/css/main.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('magnific-popup/magnific-popup.css'); ?>">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script src="<?php echo base_url('magnific-popup/jquery.magnific-popup.js'); ?>"></script>
 </head>
 <body>
 	<div class="top">
@@ -23,21 +26,12 @@
 		</div>
 	<div class="top_banner"></div>
 
-	<section class="contact_div">
-		<h1 class="contact_header">Contact Us</h1>
-		<p class="contact_one_linner">I want to thank you for becoming a user of this website.  If you have a concern or just want to thank us for making this site a awesome, please will out the form and it will email the customer service department directly.  Thank you!</p>
-		<?php echo form_open('contact/emailsupport'); ?>
-			<p>Reason to contact us</p>
-			<select name="options">
-				<option value="Problem with site">Problem with site</option>
-				<option value="Issue with content">Issue with content</option>
-				<option value="o thank us for the service">To thank us for the service</option>
-				<option value="other">Other</option>
-			</select>
-			<p>Message</p>
-			<textarea rows="20" cols="70" name="message">Leave your message here!</textarea>
-			<input type="submit" value="Send Email" class="contact_submit">
-		</form>
+	<section class="about_div">
+		<h1 class="contact_header">About Us</h1>
+		<h3 class="lead">Angel Diaz</h3>
+		<h3 class="lead_dev">Developer, Designer and Idea Maker</h3>
+		<img src="<?php echo base_url('images/me.jpg') ?>" class="lead_img"><p class="lead_info">I am web developer and web designer that was orginally suppose to be a game developer but the web called to me instead.  I am a military brat and have lived in at least 10 different states and another country by the time I was 18.  I then moved to Florida with my parents and started working at walmart.  I decided to better my life after working there for 2 years and went to Full Sail University. I graudated in June of 2013 and Tophat Gamers was my final project there.</p>
+		<p class="lead_bot">Tophat gamers is a community driven site for gamers. It gives you the ability to find new people, message them, watch their videos and read their blogs. Tophat gamers is built around the idea of making a better gamer community  by making new connections and new friendships. The site will help the world understand that the gamer community is not as bad or immature as it is given out to be. Tophatgamers.com,  come and get fancy with us.</p> 
 	</section>
 
 	<div class="all_footer_banner"></div>
